66fe704df7926d84fbbedc61ca312b4e4f44b87e
Prathmesh Singh <prathmeshsingh85@gmail.com>
2023-11-14 11:46:14 +0530

    Static Keyword

---
+++ Stage1_Static/Stage1_Static.cpp
@@ -0,0 +1,23 @@
+//Static keyword: space allocated till the end program lifetime
+/*
+  1. Static Data Members
+  2. Static Class objects  - Static class objects allows object to live till the end of the program and not only upto the scope.
+  3. Static member functions - static member functions are allowed to access only the static data members 
+*/
+
+#include <iostream>
+using namespace std;
+
+class Youtube {
+public:
+    static void showchannel() {
+        cout << "Techsido once again\n";
+    }
+};
+
+int main()
+{
+    Youtube::showchannel();
+
+    return 0;
+}
